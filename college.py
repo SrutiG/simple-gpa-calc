11b7d1a9fac9f71d62ee9c6c6806e4c36a398221
Sruti <sruti@gatech.edu>
2016-08-26 19:21:03 -0400

    added college.py and changed structure of simplegpaCalc.py

---
+++ college.py
@@ -0,0 +1,51 @@
+class Class():
+
+    gpaDict = {"A":4.0, "B":3.0, "C":2.0, "D":1.0, "F":0.0}
+
+    def __init__(self, creds, letterGrade):
+        self.creds = creds
+        self.letterGrade = letterGrade
+        self.grade = gpaDict[self.letterGrade]
+
+    def nameClass(name):
+        self.name = name
+
+class Semester():
+
+    creds = []
+    grades = []
+
+    def __init__(self, classArray)
+        self.classArray = classArray
+
+    def calculateSemesterGPA():
+        credsxgrades = 0
+        totalcreds = 0
+        for x in self.classArray:
+            creds.append(x.creds)
+            grades.append(x.grade)
+            credsxgrades = credsxgrades + x.creds*x.grade
+            totalcreds = totalcreds + x.creds
+        self.gpa = credsxgrades/totalcreds
+
+        return self.gpa
+
+class College():
+    
+    creds = []
+    grades = []
+
+    def __init__(self, semesterArray)
+        self.semesterArray = semesterArray
+
+    def calculateCollegeGPA():
+        credsxgrades = 0
+        totalcreds = 0
+        for x in self.semesterArray:
+            creds.append(x.creds)
+            grades.append(x.grade)
+            credsxgrades = credsxgrades + x.creds*x.grade
+            totalcreds = totalcreds + x.creds
+        self.gpa = credsxgrades/totalcreds
+
+        return self.gpa           
--- simplegpaCalc.py
+++ simplegpaCalc.py
@@ -1,10 +1,8 @@
 from tkinter import *
+import college
 
-class gpaCalc():
+class gpaCalcGUI():
 
-    GPAdict = {"A": 4.0, "B": 3.0, "C": 2.0, "D": 1.0, "F": 0.0}
-    grades = []
-    creds = []
 
     def __init__(self, root):
         self.root = root
@@ -19,66 +17,65 @@ class gpaCalc():
     def helperFunction(self):
         self.root.withdraw()
         self.numSemesters = int(self.semesterEty.get())
-        for x in range(self.numSemesters):
-            self.enterSemesterInfo(x)
-        self.calcGPA(grades, creds)
-        self.showGPA()
+        self.createSemesterWin(self.numSemesters)
 
-    def enterSemesterInfo(self, semesterNum):
-        self.semesterWin = Toplevel()
-        classesLbl = Label(self.semesterWin, text="How many classes did you take in semester %d?" %(semesterNum+1))
-        classesLbl.grid(row=0)
-        self.classesEty = Entry(self.semesterWin, width = 5)
-        self.classesEty.grid(row=0, column=1)
-        enterBtn = Button(self.semesterWin, text="Enter", command = self.enterClassInfo)
-        enterBtn.grid(row=1)
-
-    def enterClassInfo(self):
-        self.numClasses = int(self.classesEty.get())
-        credsLbl = Label(self.semesterWin, text="Grade")
-        credsLbl.grid(row=2, column=1)
-        gradeLbl = Label(self.semesterWin, text="Credits")
-        gradeLbl.grid(row=2, column=2)
-        self.gradeEtys = []
-        self.credEtys = []
-        for x in range(self.numClasses):
-            rownum = x + 3
-            classnumLbl = Label(self.semesterWin, text="Class %d" %(x+1))
-            classnumLbl.grid(rownum)
-            gradeEty = Entry(self.semesterWin, width=5)
-            gradeEty.grid(rownum, column=1)
-            credEty = Entry(self.semesterWin, width = 5)
-            credEty.grid(rownum, column=2)
-            self.gradeEtys.append(gradeEty)
-            self.credEtys.append(credEty)
-        enterBtn = Button(self.semesterWin, text="Enter", command = self.enterData)
-        enterBtn.grid(row=self.numClasses+3)
-
-    def enterData(self):
-        self.semesterWin.withdraw()
-        for value in self.gradeEtys:
-            grade = value.get()
-            grades.append(GPAdict[grade])
-        for item in self.credEtys:
-            cred = item.get()
-            creds.append(cred)
-        
-            
-
-    def calcGPA(self, grade, cred):
-        a = 0
-        for x in range(len(grades)):
-            a += grades[x] * creds[x]
-        totalCreds = sum(creds)
-        self.gpa = a/totalCreds
-        self.gpaStr = "{0:.2f}".format(self.gpa)
+    def createSemesterWin(self, numSemesters):
+        for x in range(numSemesters):
+            semesterWin = Toplevel()
+            semesterg = SemesterGUI(semesterWin, x+1)
+            semesterWin.mainloop           
 
     def showGPA(self):
         self.GPAWin = Toplevel()
         gpaLbl = Label(self.GPAWin, text="Your GPA is %s" %self.gpaStr)
         gpaLbl.grid(row=0)
         
-        
+class SemesterGUI():
+
+
+    def __init__(self, root, semesterNum):
+        self.root = root
+        self.semesterNum = semesterNum
+        classesLbl = Label(self.root, text="How many classes did you take in semester %d?" %self.semesterNum)
+        classesLbl.grid(row = 0, columnspan = 3)
+        self.classesEty = Entry(self.root, width = 5)
+        self.classesEty.grid(row = 0, column = 4)
+        enterBtn = Button(self.root, text="Enter", command = self.enterClassInfo)
+        enterBtn.grid(row = 1, column=5)
+
+    def enterClassInfo(self):
+        self.classes = Toplevel()
+        self.numClasses = int(self.classesEty.get())
+        credsLbl = Label(self.classes, text="Grade")
+        credsLbl.grid(row= 0, column=1)
+        gradeLbl = Label(self.classes, text="Credits")
+        gradeLbl.grid(row= 0, column=2)
+        self.gradeEtys = []
+        self.credEtys = []
+        for x in range(self.numClasses):
+            rownum = x + 1
+            classnumLbl = Label(self.classes, text="Class %d" %(x+1))
+            classnumLbl.grid(rownum)
+            gradeEty = Entry(self.classes, width=5)
+            gradeEty.grid(rownum, column=1)
+            credEty = Entry(self.classes, width = 5)
+            credEty.grid(rownum, column=2)
+            self.gradeEtys.append(gradeEty)
+            self.credEtys.append(credEty)
+        enterBtn = Button(self.classes, text="Enter", command = self.enterData)
+        enterBtn.grid(row = self.numClasses + 1)
+
+
+    def enterData(self):
+        self.classes.withdraw()
+        for value in self.gradeEtys:
+            grade = value.get()
+            grades.append(GPAdict[grade])
+        for item in self.credEtys:
+            cred = item.get()
+            creds.append(cred)
+        return 1
+
 newWin = Tk()
-loop = gpaCalc(newWin)
+loop = gpaCalcGUI(newWin)
 newWin.mainloop()           
